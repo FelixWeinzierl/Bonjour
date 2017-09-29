7cbb0971865ada66dd0095fc87f71369c30e940e
Florian Weinzierl <Florian@wzma.de>
2017-09-29 15:53:34 +0200

    index
    Added some Code to index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-asdasjd
+<?php
+	$Name = $_POST['Name'];
+	$PWD = $_POST['password'];
+	
+	echo "Das Passwort von " . $Name . " war '" . $PWD . "'";
+?>
+<html>
+	<head>
+		<title>Input</title>
+	</head>
+	<body>
+		<h1>Eingaben</h1>
+		<p>Hier sind einige Eingabefelder</p>
+		<form action="#" method="POST">
+			<label>Name:</label>
+				<input type="text" name="Name" />
+			<label>Passwort</label>
+				<input type="password" name="password" />
+			<input type="submit" value="Absenden" />
+		</form>
+	</body>
+</html>
